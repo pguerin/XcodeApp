abd6a2462d37ac867925c1d8e37a714b0b827fcb
Pascal GUERIN <pguerin@luxcloud.com>
2018-01-04 10:25:01 +0100

    Initial Commit

---
+++ XcodeApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeApp
+//
+//  Created by Pascal on 04/01/2018.
+//  Copyright © 2018 Pascal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
